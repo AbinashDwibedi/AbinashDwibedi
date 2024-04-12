0c5859984a65753588959938bcf830b1addb4531
abinashdwibedi <dwibedi.abinash123@gmail.com>
2024-04-12 15:31:09 +0530

    locomotive added

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('body'),
-//     smooth: true
-// });
-// gsap.registerPlugin(ScrollTrigger);
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('body'),
+    smooth: true
+});
+gsap.registerPlugin(ScrollTrigger);
 
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector("body"),
-//   smooth: true
-// });
-// locoScroll.on("scroll", ScrollTrigger.update);
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("body"),
+  smooth: true
+});
+locoScroll.on("scroll", ScrollTrigger.update);
 
-// ScrollTrigger.scrollerProxy("body", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
-// });
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-// ScrollTrigger.refresh();
+ScrollTrigger.scrollerProxy("body", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
+});
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
 
 let innercursor = document.querySelector(".inner-cursor");
 let outercursor = document.querySelector(".outer-cursor");
